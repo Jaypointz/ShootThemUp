5aa61b8bc8dedae58f202ce8d1248bcdb74077c5
Jaypointz <maxstep99@gmail.com>
2022-11-01 22:03:39 +0200

    Added custom damage types

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -2,6 +2,8 @@
 
 #include "Components/STUHealthComponent.h"
 #include "GameFramework/Actor.h"
+#include "Dev/STUFireDamageType.h"
+#include "Dev/STUIceDamageType.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All);
 
@@ -30,4 +32,16 @@ void USTUHealthComponent::OnTakeAnyDamageHandle(
 {
     Health -= Damage;
     UE_LOG(LogHealthComponent, Display, TEXT("Damage: %f"), Damage);
+
+    if (DamageType)
+    {
+        if (DamageType->IsA<USTUFireDamageType>())
+        {
+            UE_LOG(LogHealthComponent, Display, TEXT("SO HOT!!!!!"));
+        }
+        else if (DamageType->IsA<USTUIceDamageType>())
+        {
+            UE_LOG(LogHealthComponent, Display, TEXT("SO COLD!!!!"));
+        }
+    }
 }
