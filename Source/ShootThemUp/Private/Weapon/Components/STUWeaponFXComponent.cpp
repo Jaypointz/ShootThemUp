5f0308ed89d6ec9547876923c96c7eb7dd6c734f
Jaypointz <maxstep99@gmail.com>
2022-11-26 11:05:30 +0200

    Added decals for bullet/projectile impacts

--- Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
+++ Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
@@ -1,7 +1,10 @@
 // Shoot Them Up Game, All Rights Reserved.
 
 #include "Weapon/Components/STUWeaponFXComponent.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
 #include "NiagaraFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/DecalComponent.h"
 
 USTUWeaponFXComponent::USTUWeaponFXComponent()
 {
@@ -11,5 +14,34 @@ USTUWeaponFXComponent::USTUWeaponFXComponent()
 
 void USTUWeaponFXComponent::PlayImpactFX(const FHitResult& Hit)
 {
-    UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), Effect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
+    auto ImpactData = DefaultImpactData;
+
+    if (Hit.PhysMaterial.IsValid())
+    {
+        const auto PhysMat = Hit.PhysMaterial.Get();
+        if (ImpactDataMap.Contains(PhysMat))
+        {
+            ImpactData = ImpactDataMap[PhysMat];
+        }
+    }
+
+    // Spawn Niagara
+    UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), //
+        ImpactData.NiagaraEffect,                              //
+        Hit.ImpactPoint,                                       //
+        Hit.ImpactNormal.Rotation()                            //
+    );
+
+    // Spawn Decal
+    auto DecalComponent = UGameplayStatics::SpawnDecalAtLocation(GetWorld(),
+        ImpactData.DecalData.Material, //
+        ImpactData.DecalData.Size,     //
+        Hit.ImpactPoint,               //
+        Hit.Normal.Rotation()          //
+    );
+
+    if (DecalComponent)
+    {
+        DecalComponent->SetFadeOut(ImpactData.DecalData.LifeTime, ImpactData.DecalData.FadeOutTime);
+    }
 }
--- Source/ShootThemUp/Public/Weapon/Components/STUWeaponFXComponent.h
+++ Source/ShootThemUp/Public/Weapon/Components/STUWeaponFXComponent.h
@@ -4,9 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "STUCoreTypes.h"
 #include "STUWeaponFXComponent.generated.h"
 
 class UNiagaraSystem;
+class UPhysicalMaterial;
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SHOOTTHEMUP_API USTUWeaponFXComponent : public UActorComponent
@@ -20,5 +22,8 @@ public:
 
 protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
-    UNiagaraSystem* Effect;
+    FImpactData DefaultImpactData;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+    TMap<UPhysicalMaterial*, FImpactData> ImpactDataMap;
 };
