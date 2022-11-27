2d67d61c44815de5c259113a5a42f115e99e842f
Jaypointz <maxstep99@gmail.com>
2022-11-27 02:31:19 +0200

    Commented out launcher explosion debug sphere

--- Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
+++ Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
@@ -70,7 +70,7 @@ void ASTUProjectile::OnProjectileHit(
         GetController(),                            //
         DoFullDamage);
 
-    DrawDebugSphere(GetWorld(), GetActorLocation(), DamageRadius, 24, FColor::Red, false, 5.0f);
+    //DrawDebugSphere(GetWorld(), GetActorLocation(), DamageRadius, 24, FColor::Red, false, 5.0f);
 
     WeaponFX->PlayImpactFX(Hit);
 
